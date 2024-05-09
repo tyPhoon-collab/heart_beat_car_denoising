68d4c61fa2d80d6f0a089e7deeededb571f7e176
tyPhoon <typhoonmoon1022@gmail.com>
2024-05-09 15:43:52 +0900

    Change:細かな修正、訓練コードのリファクタリング

--- train.py
+++ train.py
@@ -1,44 +1,41 @@
-from dataset import NoisyHeartbeatDataset
-from models.wave_u_net import WaveUNet
+import torch
+from torch.utils.data import DataLoader
 import torch.optim as optim
 import torch.nn as nn
-from torch.utils.data import DataLoader
-
+from dataset import NoisyHeartbeatDataset
+from models.wave_u_net import WaveUNet
 from randomizer import NumpyRandomShuffleRandomizer
 from sampling_rate_converter import ScipySamplingRateConverter
 
-model = WaveUNet()
 
+def train_model(model, dataloader, criterion, optimizer, num_epochs=5):
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        for noisy, clean in dataloader:
+            optimizer.zero_grad()
+            outputs = model(noisy)
+            loss = criterion(outputs, clean)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        epoch_loss = running_loss / len(dataloader)
+        print(f"Epoch {epoch + 1}, Loss: {epoch_loss:.4f}")
+        torch.save(model.state_dict(), f"checkpoints/model_weights_epoch_{epoch}.pth")
+
+
+model = WaveUNet()
 dataset = NoisyHeartbeatDataset(
     clean_file_path="data/Stop.mat",
     noisy_file_path="data/100km.mat",
-    # noisy_file_path="data/Stop.mat",
     sampling_rate_converter=ScipySamplingRateConverter(
-        input_rate=32000,
-        output_rate=1024,
+        input_rate=32000, output_rate=1024
     ),
     randomizer=NumpyRandomShuffleRandomizer(),
 )
-dataloader = DataLoader(
-    dataset,
-    batch_size=1,
-    shuffle=True,
-)
-
+dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
 criterion = nn.L1Loss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-num_epochs = 5
-
-for epoch in range(num_epochs):
-    for noisy, clean in dataloader:
-        optimizer.zero_grad()
-        outputs = model(noisy)
-        loss = criterion(outputs, clean)
-        loss.backward()
-
-        optimizer.step()
-
-        # print(loss)
-
-    print(f"Epoch {epoch + 1}, Loss: {loss.item()}")  # type: ignore
+train_model(model, dataloader, criterion, optimizer, num_epochs=5)
