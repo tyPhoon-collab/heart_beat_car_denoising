198fa7d2e9fc66bb8fc3eba3fa217b41c5971029
tyPhoon <typhoonmoon1022@gmail.com>
2024-05-09 11:22:34 +0900

    Add:モデルと訓練コードの仮追加

---
+++ models/wave_u_net.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class WaveUNet(nn.Module):
+    def __init__(self):
+        super(WaveUNet, self).__init__()
+        self.encoder_layers = nn.ModuleList(
+            [
+                nn.Conv1d(
+                    in_channels=1, out_channels=16, kernel_size=15, stride=2, padding=7
+                ),
+                nn.Conv1d(16, 32, 15, 2, 7),
+                nn.Conv1d(32, 64, 15, 2, 7),
+                nn.Conv1d(64, 128, 15, 2, 7),
+                nn.Conv1d(128, 256, 15, 2, 7),
+                nn.Conv1d(256, 512, 15, 2, 7),
+                nn.Conv1d(512, 1024, 15, 2, 7),
+                nn.Conv1d(1024, 1024, 15, 2, 7),
+            ]
+        )
+        self.decoder_layers = nn.ModuleList(
+            [
+                nn.ConvTranspose1d(1024, 1024, 15, 2, 7, output_padding=1),
+                nn.ConvTranspose1d(2048, 512, 15, 2, 7, output_padding=1),
+                nn.ConvTranspose1d(1024, 256, 15, 2, 7, output_padding=1),
+                nn.ConvTranspose1d(512, 128, 15, 2, 7, output_padding=1),
+                nn.ConvTranspose1d(256, 64, 15, 2, 7, output_padding=1),
+                nn.ConvTranspose1d(128, 32, 15, 2, 7, output_padding=1),
+                nn.ConvTranspose1d(64, 16, 15, 2, 7, output_padding=1),
+                nn.ConvTranspose1d(32, 1, 15, 2, 7, output_padding=1),
+            ]
+        )
+
+    def forward(self, x):
+        # Encoder
+        skip_connections = []
+        for layer in self.encoder_layers:
+            x = F.leaky_relu(layer(x), 0.2)
+            skip_connections.append(x)
+
+        skip_connections = skip_connections[::-1]
+        # Decoder
+        for i, layer in enumerate(self.decoder_layers):
+            x = F.leaky_relu(layer(x), 0.2)
+            if (
+                i < len(self.decoder_layers) - 1
+            ):  # Skip connection for all except last layer
+                x = torch.cat(
+                    [x, skip_connections[i + 1]], 1
+                )  # Skip the first connection
+        return x
+
+
+# Create model and example input tensor
+model = WaveUNet()
+example_input = torch.rand(1, 1, 256 * 20)  # (batch_size, channels, length)
+# Get the model output
+output = model(example_input)
+print(output.shape)  # Should be torch.Size([1, 1, 5000])
---
+++ train.py
@@ -0,0 +1,42 @@
+from dataset import NoisyHeartbeatDataset
+from models.wave_u_net import WaveUNet
+import torch.optim as optim
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from randomizer import NumpyRandomShuffleRandomizer
+from sampling_rate_converter import ScipySamplingRateConverter
+
+model = WaveUNet()
+
+dataset = NoisyHeartbeatDataset(
+    clean_file_path="data/Stop.mat",
+    noisy_file_path="data/100km.mat",
+    # noisy_file_path="data/Stop.mat",
+    sampling_rate_converter=ScipySamplingRateConverter(
+        input_rate=32000,
+        output_rate=1000,
+    ),
+    randomizer=NumpyRandomShuffleRandomizer(),
+)
+dataloader = DataLoader(
+    dataset,
+    batch_size=1,
+    shuffle=True,
+)
+
+criterion = nn.L1Loss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 5
+
+for epoch in range(num_epochs):
+    for inputs, labels in dataloader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+
+        optimizer.step()
+
+    print(f"Epoch {epoch + 1}, Loss: {loss.item()}")  # type: ignore
