b28dec0039b27b1567750bcc04b8a4f9360859a4
tyPhoon <typhoonmoon1022@gmail.com>
2024-05-16 01:39:46 +0900

    Change:細かな変更

--- eval.py
+++ eval.py
@@ -9,6 +9,7 @@ from utils.device import get_device_name
 from utils.plot import plot_three_signals
 from utils.sound import save_signal_to_wav_scipy
 from utils.timeit import timeit
+from numpy.typing import ArrayLike
 
 
 @timeit
@@ -44,40 +45,72 @@ def eval_model(
             cpu_clean = clean[0][0].cpu().numpy()
             cpu_outputs = outputs[0][0].cpu().numpy()
 
-            sample_rate = 1024
+            save_figures(
+                figure_filename,
+                cpu_noisy,
+                cpu_clean,
+                cpu_outputs,
+            )
 
-            if figure_filename is not None:
-                plot_three_signals(
-                    cpu_noisy,
-                    cpu_clean,
-                    cpu_outputs,
-                    "Noisy",
-                    "Clean",
-                    "Output",
-                    filename=figure_filename,
-                )
+            save_audio_files(
+                clean_audio_filename,
+                noisy_audio_filename,
+                audio_filename,
+                cpu_noisy,
+                cpu_clean,
+                cpu_outputs,
+            )
+            break
 
-            if clean_audio_filename is not None:
-                save_signal_to_wav_scipy(
-                    cpu_clean,
-                    filename=clean_audio_filename,
-                    sample_rate=sample_rate,
-                )
 
-            if noisy_audio_filename is not None:
-                save_signal_to_wav_scipy(
-                    cpu_noisy,
-                    filename=noisy_audio_filename,
-                    sample_rate=sample_rate,
-                )
+def save_figures(
+    figure_filename: str | None,
+    cpu_noisy: ArrayLike,
+    cpu_clean: ArrayLike,
+    cpu_outputs: ArrayLike,
+):
+    if figure_filename is not None:
+        plot_three_signals(
+            cpu_noisy,
+            cpu_clean,
+            cpu_outputs,
+            "Noisy",
+            "Clean",
+            "Output",
+            filename=figure_filename,
+        )
 
-            if audio_filename is not None:
-                save_signal_to_wav_scipy(
-                    cpu_outputs,
-                    filename=audio_filename,
-                    sample_rate=sample_rate,
-                )
-            break  # 最初のバッチのみ処理
+
+def save_audio_files(
+    clean_audio_filename: str | None,
+    noisy_audio_filename: str | None,
+    audio_filename: str | None,
+    cpu_noisy: ArrayLike,
+    cpu_clean: ArrayLike,
+    cpu_outputs: ArrayLike,
+):
+    sample_rate = 1024
+
+    if clean_audio_filename is not None:
+        save_signal_to_wav_scipy(
+            cpu_clean,
+            filename=clean_audio_filename,
+            sample_rate=sample_rate,
+        )
+
+    if noisy_audio_filename is not None:
+        save_signal_to_wav_scipy(
+            cpu_noisy,
+            filename=noisy_audio_filename,
+            sample_rate=sample_rate,
+        )
+
+    if audio_filename is not None:
+        save_signal_to_wav_scipy(
+            cpu_outputs,
+            filename=audio_filename,
+            sample_rate=sample_rate,
+        )  # 最初のバッチのみ処理
 
 
 if __name__ == "__main__":
--- utils/sound.py
+++ utils/sound.py
@@ -1,8 +1,12 @@
 import numpy as np
+from numpy.typing import ArrayLike
 from scipy.io import wavfile
 
 
-def save_signal_to_wav_scipy(data, sample_rate, filename):
-    # 最大振幅を16ビット整数の範囲にスケール
+def save_signal_to_wav_scipy(
+    data: ArrayLike,
+    sample_rate: int,
+    filename: str,
+):
     scaled_data = np.int16(data / np.max(np.abs(data)) * 32767)
     wavfile.write(f"output/audio/{filename}", sample_rate, scaled_data)
