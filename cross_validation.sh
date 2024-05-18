993da5d74780ce3ff42ec8030d1106f6bf310159
tyPhoon <typhoonmoon1022@gmail.com>
2024-05-18 19:06:24 +0900

    Change:ID名の変更

--- cross_validation.sh
+++ cross_validation.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-bash train_and_eval.sh WaveUNet_L1 WaveUNet L1Loss
-bash train_and_eval.sh WaveUNet_Smooth_L1 WaveUNet SmoothL1Loss
+bash train_and_eval.sh WUN_L1 WaveUNet L1Loss
+bash train_and_eval.sh WUN_SL1 WaveUNet SmoothL1Loss
 
-bash train_and_eval.sh Autoencoder_L1_SampleShuffle Conv1DAutoencoder L1Loss --randomizer SampleShuffleRandomizer
-bash train_and_eval.sh Autoencoder_Smooth_L1_SampleShuffle Conv1DAutoencoder SmoothL1Loss --randomizer SampleShuffleRandomizer
+bash train_and_eval.sh AE_L1_SS Conv1DAutoencoder L1Loss --randomizer SampleShuffleRandomizer
+bash train_and_eval.sh AE_SL1_SS Conv1DAutoencoder SmoothL1Loss --randomizer SampleShuffleRandomizer
 
-bash train_and_eval.sh Autoencoder_L1 Conv1DAutoencoder L1Loss
-bash train_and_eval.sh Autoencoder_Smooth_L1 Conv1DAutoencoder SmoothL1Loss
+bash train_and_eval.sh AE_L1 Conv1DAutoencoder L1Loss
+bash train_and_eval.sh AE_SL1 Conv1DAutoencoder SmoothL1Loss
 
-bash train_and_eval.sh Autoencoder_L1 Conv1DAutoencoder L1Loss --with-progressive-gain
-bash train_and_eval.sh Autoencoder_Smooth_L1 Conv1DAutoencoder SmoothL1Loss --with-progressive-gain
+bash train_and_eval.sh P_AE_L1 Conv1DAutoencoder L1Loss --with-progressive-gain
+bash train_and_eval.sh P_AE_SL1 Conv1DAutoencoder SmoothL1Loss --with-progressive-gain
 
-bash train_and_eval.sh ShuffleAutoencoder_L1 PixelShuffleConv1DAutoencoder L1Loss
-bash train_and_eval.sh ShuffleAutoencoder_Smooth_L1 PixelShuffleConv1DAutoencoder SmoothL1Loss
+bash train_and_eval.sh SAE_L1 PixelShuffleConv1DAutoencoder L1Loss
+bash train_and_eval.sh SAE_SL1 PixelShuffleConv1DAutoencoder SmoothL1Loss
 
-bash train_and_eval.sh TransformerShuffleAutoencoder_L1 PixelShuffleConv1DAutoencoderWithTransformer L1Loss
-bash train_and_eval.sh TransformerShuffleAutoencoder_Smooth_L1 PixelShuffleConv1DAutoencoderWithTransformer SmoothL1Loss
+bash train_and_eval.sh SAET_L1 PixelShuffleConv1DAutoencoderWithTransformer L1Loss
+bash train_and_eval.sh SAET_SL1 PixelShuffleConv1DAutoencoderWithTransformer SmoothL1Loss
