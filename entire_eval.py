145d0f229f2734cb8bdc690350e8f9ad1bb6fee0
tyPhoon <typhoonmoon1022@gmail.com>
2024-11-09 14:49:21 +0900

    Fix:entire_evalを修正

--- entire_eval.py
+++ entire_eval.py
@@ -1,5 +1,3 @@
-import numpy as np
-import torch
 from torch.nn import L1Loss
 from torch.utils.data import DataLoader
 from dataset.factory import DatasetFactory
@@ -8,7 +6,9 @@ from logger.evaluation_impls.composite import CompositeEvaluationLogger
 from logger.evaluation_impls.figure import FigureEvaluationLogger
 from logger.evaluation_impls.plotly import PlotlyEvaluationLogger
 from logger.evaluation_impls.stdout import StdoutEvaluationLogger
+from loss.weighted import WeightedLoss
 from models.wave_u_net_enhance_transformer import WaveUNetEnhanceTransformer
+from solver import SimpleSolver
 from utils.device import get_torch_device
 
 weights_path = "output/checkpoint/model_weights_best.pth"
@@ -16,9 +16,9 @@ weights_path = "output/checkpoint/model_weights_best.pth"
 device = get_torch_device()
 
 model = WaveUNetEnhanceTransformer()
+solver = SimpleSolver(model, WeightedLoss())
+
 model.to(device)
-model.load_state_dict(torch.load(weights_path))
-model.eval()
 
 logger = CompositeEvaluationLogger(
     [
@@ -36,13 +36,6 @@ logger = CompositeEvaluationLogger(
 
 criterion = L1Loss()
 
-total_loss = 0.0
-count = 0
-
-noisy_tensors = []
-clean_tensors = []
-outputs_tensors = []
-
 dataset = DatasetFactory.create_240517_entire_noise()
 dataloader = DataLoader(
     dataset,
@@ -50,26 +43,9 @@ dataloader = DataLoader(
     shuffle=False,
 )
 
-with torch.no_grad():
-    for batch in dataloader:
-        noisy, clean = map(lambda x: x.to(device), batch)
-        outputs = model(noisy)
-
-        loss = criterion(outputs, clean)
-        total_loss += loss.item()
-        count += 1
-
-        noisy_tensors.append(noisy[0][0].cpu().numpy())
-        clean_tensors.append(clean[0][0].cpu().numpy())
-        outputs_tensors.append(outputs[0][0].cpu().numpy())
-
-concat_noisy = np.concatenate(noisy_tensors)
-concat_clean = np.concatenate(clean_tensors)
-concat_outputs = np.concatenate(outputs_tensors)
-
-logger.on_data(
-    concat_noisy,
-    concat_clean,
-    concat_outputs,
+solver.evaluate(
+    dataloader=dataloader,
+    criterion=criterion,
+    state_dict_path=weights_path,
+    logger=logger,
 )
-logger.on_average_loss(total_loss / count)
